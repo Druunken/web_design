f2127e613ff9e7b912aa98e0461894a705ad31a9
druunken <Oguzhanalbayrak444@gmail.com>
2025-02-25 22:54:07 +0100

    first commit

---
+++ public/content.js
@@ -0,0 +1,56 @@
+export const whyPomContent = `
+        <div id="contentModal">
+            <p class="label-header">Why pOmPom?</p>
+            <div class="modal-content-div">
+                <p class="nav-label">
+                    Cuz i think he is the cutiest dogs i've seen and
+                </p>
+                <p class="nav-label">
+                    it's free so u can drop it anytime.
+                </p>
+            </div>
+        </div>
+    `
+export const platformContent = `
+        <div id="contentModal">
+            <p class="label-header">Platform.</p>
+            <div class="modal-content-div">
+                <p class="nav-label">
+                    IOS
+                </p>
+                <p class="nav-label">
+                    Android
+                </p>
+            </div>
+        </div>
+    `
+export const currencyContent = `
+        <div id="contentModal">
+            <p class="label-header">Currencies.</p>
+            <div class="modal-content-div">
+                <p class="nav-label">
+                    US-Dollar
+                </p>
+                <p class="nav-label">
+                    Euro
+                </p>
+                <p class="nav-label">
+                    Japanese-Yen
+                </p>
+                <p class="nav-label">
+                    Pound
+                </p>
+            </div>
+        </div>
+    `
+
+export const changelogContent = `
+    <div id="contentModal">
+        <p class="label-header">Changelog.</p>
+        <div class="modal-content-div">
+                <p class="nav-label">
+                    Version 0.1.0.0
+                </p>
+            </div>
+    </div>
+`
--- public/index.js
+++ public/index.js
@@ -1,7 +1,13 @@
+import { whyPomContent,
+    platformContent,
+    currencyContent,
+    changelogContent } from './content.js'
+
 
 const contentModal = document.getElementById("modal")
 const titleDiv = document.querySelector(".title-div")
 const trialDiv = document.querySelector(".trial-div")
+
 
 
 const rmAcitve = () => {
@@ -11,26 +17,42 @@ const rmAcitve = () => {
     })
 }
 
+
+const showContent = (content,msg,ev) => {
+    rmAcitve()
+    const childElement = document.getElementById("contentModal")
+    if(childElement) contentModal.removeChild(childElement)
+    ev.target.classList.add("active")
+    titleDiv.style.display = "none"
+    trialDiv.style.display = "none"
+    contentModal.classList.add("active")
+
+    contentModal.insertAdjacentHTML("beforeend",content)
+
+    console.log(msg)
+}
+
 document.addEventListener("click",(ev) => {
     if(ev.target.innerText === "Why pomExpen$e?"){
-        rmAcitve()
-        ev.target.classList.add("active")
-        titleDiv.style.display = "none"
-        trialDiv.style.display = "none"
-        contentModal.classList.add("active")
-        console.log(titleDiv.classList)
-        console.log("Why pom expenses baby")
+        showContent(whyPomContent,"Why pom expenses baby",ev)
+
     }else if(ev.target.innerText === "Platform"){
-        rmAcitve()
-        ev.target.classList.add("active")
-        console.log("Platform")
+        showContent(platformContent,"Platform",ev)
+
     }else if(ev.target.innerText === "Solutions"){
-        rmAcitve()
-        ev.target.classList.add("active")
-        console.log("Solutions")
+        showContent(currencyContent,"Solutions",ev)
+        
     }else if(ev.target.innerText === "Changelog"){
+        showContent(changelogContent,"Changelog",ev)
+
+    }else if(ev.target.id.includes("main-title")){
         rmAcitve()
-        ev.target.classList.add("active")
-        console.log("Changelog")
+
+        titleDiv.style.display = "flex"
+        trialDiv.style.display = "flex"
+
+        const childElement = document.getElementById("contentModal")
+        if(childElement) contentModal.removeChild(childElement)
+
     }
 })
